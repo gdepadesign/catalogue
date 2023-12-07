d6f0c7156da4b891c82fb9e385206723922d9975
Giacomo <reddy.depa@gmail.com>
2023-12-07 23:29:36 +0100

    preventDefault fix

--- js/script.js
+++ js/script.js
@@ -144,18 +144,16 @@ function changeProject() {
 
 function swipeRight() {
   $(document.body).on(
-    "swiperight",
-    {passive: false},
-    function () {
-      $(".wrapper").removeClass("is-shift-content");
-      $(".nav").removeClass("is-shift-content");
-      $(".main").removeClass("is-shift-content");
-      $(".headline").removeClass("is-shift-content");
-      $(".sidebar").removeClass("is-shift-sidebar");
-      $(".open").removeClass("open-active");
-      $(".project").removeClass("project-active");
-      $(".project-name").removeClass("open-active");
-      $(".open").removeClass("nohover");
+    "swiperight", function (e) {
+    $(".wrapper").removeClass("is-shift-content");
+    $(".nav").removeClass("is-shift-content");
+    $(".main").removeClass("is-shift-content");
+    $(".headline").removeClass("is-shift-content");
+    $(".sidebar").removeClass("is-shift-sidebar");
+    $(".open").removeClass("open-active");
+    $(".project").removeClass("project-active");
+    $(".project-name").removeClass("open-active");
+    $(".open").removeClass("nohover");
   });
 }
 
@@ -467,6 +465,72 @@ function addProgressBar(dataTarget) {
     $(".progress").width(scrollPercent + "%");
   });
 }
+
+// Open preventDefault fix
+
+const eventListenerOptionsSupported = () => {
+  let supported = false;
+
+  try {
+    const opts = Object.defineProperty({}, 'passive', {
+      get() {
+        supported = true;
+      }
+    });
+
+    window.addEventListener('test', null, opts);
+    window.removeEventListener('test', null, opts);
+  } catch (e) {}
+
+  return supported;
+}
+
+const defaultOptions = {
+  passive: false,
+  capture: false
+};
+const supportedPassiveTypes = [
+  'scroll', 'wheel',
+  'touchstart', 'touchmove', 'touchenter', 'touchend', 'touchleave',
+  'mouseout', 'mouseleave', 'mouseup', 'mousedown', 'mousemove', 'mouseenter', 'mousewheel', 'mouseover'
+];
+const getDefaultPassiveOption = (passive, eventName) => {
+  if (passive !== undefined) return passive;
+
+  return supportedPassiveTypes.indexOf(eventName) === -1 ? false : defaultOptions.passive;
+};
+
+const getWritableOptions = (options) => {
+  const passiveDescriptor = Object.getOwnPropertyDescriptor(options, 'passive');
+
+  return passiveDescriptor && passiveDescriptor.writable !== true && passiveDescriptor.set === undefined
+    ? Object.assign({}, options)
+    : options;
+};
+
+const overwriteAddEvent = (superMethod) => {
+  EventTarget.prototype.addEventListener = function (type, listener, options) {
+    const usesListenerOptions = typeof options === 'object' && options !== null;
+    const useCapture          = usesListenerOptions ? options.capture : options;
+
+    options         = usesListenerOptions ? getWritableOptions(options) : {};
+    options.passive = getDefaultPassiveOption(options.passive, type);
+    options.capture = useCapture === undefined ? defaultOptions.capture : useCapture;
+
+    superMethod.call(this, type, listener, options);
+  };
+
+  EventTarget.prototype.addEventListener._original = superMethod;
+};
+
+const supportsPassive = eventListenerOptionsSupported();
+
+if (supportsPassive) {
+  const addEvent = EventTarget.prototype.addEventListener;
+  overwriteAddEvent(addEvent);
+}
+
+// Close preventDefault fix
 
 document.addEventListener("DOMContentLoaded", () => {
   addStickyHeader();
